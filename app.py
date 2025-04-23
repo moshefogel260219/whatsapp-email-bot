419c600ac2367b24a940798ccc98554ed09161ce
moshefogel260219 <moshfog@gmail.com>
2025-04-23 07:17:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -56,6 +56,11 @@ def send_email(subject, body, media_url=None, media_type=None):
     print(f"SendGrid response code: {response.status_code}")
     print(f"NumMedia: {request.form.get('NumMedia')}")
     print(f"SendGrid response body: {response.body}")
+print("------ Full request.form ------")
+for key in request.form:
+    print(f"{key} => {request.form[key]}")
+    print("-------------------------------")
+    print(f"Content length: {len(response.content)} bytes")
 
 @app.route("/whatsapp", methods=["POST"])
 def whatsapp_webhook():
